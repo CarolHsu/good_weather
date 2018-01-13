c551389787e8bd66f112a4e8f2117e3eb3197d7b
shesee <sheseee@gmail.com>
2018-01-14 02:02:11 +0800

    Add search input for searching by city name

--- spec/views/weather/info.html.erb_spec.rb
+++ spec/views/weather/info.html.erb_spec.rb
@@ -5,10 +5,16 @@ describe 'weather/info.html.erb' do
   context "got successful response" do
     let(:fake_info) { JSON.parse '{"coord":{"lon":145.77,"lat":-16.92},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":300.15,"pressure":1007,"humidity":74,"temp_min":300.15,"temp_max":300.15},"visibility":10000,"wind":{"speed":3.6,"deg":160},"clouds":{"all":40},"dt":1485790200,"sys":{"type":1,"id":8166,"message":0.2064,"country":"AU","sunrise":1485720272,"sunset":1485766550},"id":2172797,"name":"Cairns","cod":200}' }
     
+    it 'should have a search input' do
+      assign(:information, fake_info)
+      render
+      expect(rendered).to have_selector('div#search')
+    end
+
     it 'should render city name' do
       assign(:information, fake_info)
       render
-      expect(rendered).to have_selector('div#information', text: fake_info["name"] )
+      expect(rendered).to have_selector('input#cityName')
     end
 
     it 'should render country code' do
@@ -31,4 +37,5 @@ describe 'weather/info.html.erb' do
       expect(rendered).not_to have_selector('div#information')
     end
   end
+
 end
