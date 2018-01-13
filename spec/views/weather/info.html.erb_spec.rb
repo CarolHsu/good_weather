37cd81628eb6cce6f39dc1aca450dee8f3b991fd
shesee <sheseee@gmail.com>
2018-01-13 17:42:08 +0800

    Root page should have weather information

---
+++ spec/views/weather/info.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'json'
+
+describe 'weather/info.html.erb' do
+  let(:fake_info) { JSON.parse '{"coord":{"lon":145.77,"lat":-16.92},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":300.15,"pressure":1007,"humidity":74,"temp_min":300.15,"temp_max":300.15},"visibility":10000,"wind":{"speed":3.6,"deg":160},"clouds":{"all":40},"dt":1485790200,"sys":{"type":1,"id":8166,"message":0.2064,"country":"AU","sunrise":1485720272,"sunset":1485766550},"id":2172797,"name":"Cairns","cod":200}' }
+  
+  it 'should render city name' do
+    assign(:information, fake_info)
+    render
+    expect(rendered).to have_selector('div#information', text: fake_info["name"] )
+  end
+
+  it 'should render country code' do
+    assign(:information, fake_info)
+    render
+    expect(rendered).to have_selector('div#information', text: fake_info["sys"]["country"])
+  end
+
+  it 'should render weather description' do
+    assign(:information, fake_info)
+    render
+    expect(rendered).to have_selector('div#information', text: fake_info["weather"][0]["description"])
+  end
+end
