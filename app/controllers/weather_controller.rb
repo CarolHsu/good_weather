91cbf3c0be7ecc3d36261c94a02cfae2c98fffa0
shesee <sheseee@gmail.com>
2018-01-13 18:20:59 +0800

    Add random city function

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,5 +1,15 @@
 class WeatherController < ApplicationController
   def info
-    @information = JSON.parse '{"coord":{"lon":145.77,"lat":-16.92},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":300.15,"pressure":1007,"humidity":74,"temp_min":300.15,"temp_max":300.15},"visibility":10000,"wind":{"speed":3.6,"deg":160},"clouds":{"all":40},"dt":1485790200,"sys":{"type":1,"id":8166,"message":0.2064,"country":"AU","sunrise":1485720272,"sunset":1485766550},"id":2172797,"name":"Cairns","cod":200}'
+    #TODO implement OpenWeatherAPI::Client in lib
+    @information = OpenWeatherAPI::Client.new.get(city_id: seed_city_id)
+  end
+
+  private
+
+  def seed_city_id
+    file = File.join(Rails.root, 'config', 'city.list.json')
+    cities = JSON.parse File.read(file)
+    city_ids = cities.map { |c| c["id"] }
+    city_ids.sample
   end
 end
--- spec/controllers/weather_controller_spec.rb
+++ spec/controllers/weather_controller_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe WeatherController, type: :controller do
 
   describe 'Info' do
+    before :each do
+      srand(1)
+    end
 
     it 'should render a new weather information template' do
       get :info
@@ -13,6 +16,12 @@ RSpec.describe WeatherController, type: :controller do
       # Pending
       # get :info
       # expect(assigns[:information]).to be_a_new WeatherAPI::Client
+    end
+
+    it "should get a random city id" do
+      controller = WeatherController.new
+      seed_city_id = controller.send(:seed_city_id)
+      expect(seed_city_id).to eq(1815202)
     end
 
   end
